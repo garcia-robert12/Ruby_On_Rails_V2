292c5198ac9c514bd48a26b31b2b5b9cbe04d475
Robert Garcia <garcia.robert12@aol.com>
2018-09-20 11:03:12 -0700

    Update Apache_Script_Edit.rb

--- Apache_Script_Edit.rb
+++ Apache_Script_Edit.rb
@@ -1,20 +1,3 @@
-#!/usr/bin/ruby
-#
-# Apache2 log analyzer to count unique ips and number of queries
-#
-# This script is used to find the unique ips in an apache2 access.log
-# and count the number of queries for each ip, the number of errors served
-# and the number of queries to secret.html for each ip.
-#
-#   ApacheLogAnalyzer: Analyzer for a log file given the full or relative path
-#
-# Apache2 log analyzer
-#
-#  Build an instance of this class and pass a list of full or relative path(s)
-#  to the analyze function to receive a list of unique IP addresses, the total
-#  number of queries per IP, the number of queries to secret.html per IP, and
-#  the total number of 404 errors served.
-#
 class ApacheLogAnalyzer
   def initialize
     @total_hits_by_ip = Hash.new(0)
@@ -22,11 +5,6 @@ class ApacheLogAnalyzer
     @secret_hits_by_ip = Hash.new(0)
     @error_count = 0
   end
-  # Analyze a log file provided the full or relative path
-  #
-  # Args:
-  # - file_name: string -- Full or relative path to a log file
-  #
   def analyze(file_name)
     # Regex to match a single octet of an IPv4 address
     octet = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
@@ -39,37 +17,22 @@ class ApacheLogAnalyzer
     # TODO: Match the various regex (IP Address, URL, Secret URL, and 404 Error)
     # and pass them to the count_hits function to be counted
     File.open(file_name).each do |line|
-         ip = ip_regex.match(line)[0]
-         url = url_regex.match(line)[0]
+         ip = ip_regex.match(line).to_s
+         url = url_regex.match(line).to_s
          secret = line.include?("secret")
          error = line.include?("404")
         count_hits(ip, url, secret, error)
-    end
+  end
     print_hits
   end
   
-  private
-  # Count the total and secret queries for a given ip
-  #
-  # Args:
-  # - ip: string -- IP address responsible for the logged entry
-  # - url: string -- URL queried for the logged entry
-  # - secret: bool -- Whether or not the url queried was secret.html
-  # - error: bool -- Whether or not the log entry contained a 404 error
-  #
+    private
   def count_hits(ip, url, secret, error)
     @total_hits_by_ip[ip] += 1
     @total_hits_per_url[url] += 1
     @secret_hits_by_ip[ip] += 1 if secret
     @error_count += 1 if error
-    end
-    # TODO: Associate the request with the IP Address
-    # TODO: Associate the request with the url requested
-    # TODO: Associate the request with the IP if the query is for the Secret URL
-    # TODO: Keep track of the total number of 404 errors served
-  
-  # Print the number of queries for each ip to the secret url and in total
-  #
+  end
   def print_hits
     print_string = 'IP: %s, Total Hits: %s, Secret Hits: %s'
     @total_hits_by_ip.sort.each do |ip, total_hits|
@@ -82,7 +45,7 @@ class ApacheLogAnalyzer
     end
     puts sprintf('Total Errors: %s', @error_count)
   end
-end
+ end
 def usage
   puts "No log files passed, please pass at least one log file.\n\n"
   puts "USAGE: #{$PROGRAM_NAME} file1 [file2 ...]\n\n"
@@ -95,9 +58,3 @@ def main
   end
   ARGV.each do |file_name|
     log_analyzer = ApacheLogAnalyzer.new
-    log_analyzer.analyze(file_name)
-  end
-end
-if __FILE__ == $PROGRAM_NAME
-  main
-end
