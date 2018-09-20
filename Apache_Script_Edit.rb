7c4a846a25de8a111bbac6b2c5624ff978f81b1b
Robert Garcia <garcia.robert12@aol.com>
2018-09-20 11:04:46 -0700

    Final_Edit
    This is the winner!!!!

--- Apache_Script_Edit.rb
+++ Apache_Script_Edit.rb
@@ -6,16 +6,9 @@ class ApacheLogAnalyzer
     @error_count = 0
   end
   def analyze(file_name)
-    # Regex to match a single octet of an IPv4 address
     octet = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
-    # Since an IPv4 address is made of four octets we will string them together
-    # to match a full IPv4 address
     ip_regex = /^#{octet}\.#{octet}\.#{octet}\.#{octet}/
-    # Regex to match an alphanumeric url ending with .html
     url_regex = /[a-zA-Z0-9]+.html/
-    # TODO: Read in the file line by line using a loop
-    # TODO: Match the various regex (IP Address, URL, Secret URL, and 404 Error)
-    # and pass them to the count_hits function to be counted
     File.open(file_name).each do |line|
          ip = ip_regex.match(line).to_s
          url = url_regex.match(line).to_s
@@ -38,6 +31,32 @@ class ApacheLogAnalyzer
     @total_hits_by_ip.sort.each do |ip, total_hits|
       secret_hits = @secret_hits_by_ip[ip]
       puts sprintf(print_string, ip, total_hits, secret_hits)
+    end
+    url_print_string = 'URL: %s, Number of Hits: %s'
+    @total_hits_per_url.sort.each do |url, url_hits|
+      puts sprintf(url_print_string, url, url_hits)
+    end
+    puts sprintf('Total Errors: %s', @error_count)
+  end
+ end
+def usage
+  puts "No log files passed, please pass at least one log file.\n\n"
+  puts "USAGE: #{$PROGRAM_NAME} file1 [file2 ...]\n\n"
+  puts "Analyzes apache2 log files for unique IP addresses and unique URLs."
+end
+def main
+  if ARGV.empty?
+    usage
+    exit(1)
+  end
+  ARGV.each do |file_name|
+    log_analyzer = ApacheLogAnalyzer.new
+    log_analyzer.analyze(file_name)
+  end
+end
+if __FILE__ == $PROGRAM_NAME
+  main
+end
     end
     url_print_string = 'URL: %s, Number of Hits: %s'
     @total_hits_per_url.sort.each do |url, url_hits|
